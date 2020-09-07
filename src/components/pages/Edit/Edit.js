a78edcfb08c9e2d79505714e414c25c6f536c004
Joshua Medlen <joshua246@gmail.com>
2020-09-07 09:06:38 -0500

    single view and edit view added and working

--- src/components/pages/Edit/Edit.js
+++ src/components/pages/Edit/Edit.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 class Edit extends React.Component {
   render() {
+    const { itemId } = this.props.match.params;
     return (
       <div className="Edit">
         <h1>Edit</h1>
+        <p>this is {itemId}</p>
       </div>
     );
   }
--- src/components/shared/BuildItems/BuildItems.js
+++ src/components/shared/BuildItems/BuildItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import itemShape from '../../../helpers/propz/itemShape';
 
@@ -10,12 +11,17 @@ class BuildItems extends React.Component {
   render() {
     const { items } = this.props;
 
+    const editLink = `/edit/${items.id}`;
+    const singleItem = `/stuffs/${items.id}`;
+
     return (
       <div className="card" >
         <img className="card-img-top" src={items.itemImage} alt={items.id} />
         <div className="card-body">
           <h5 className="card-title">{items.itemName}</h5>
           <p className="card-text">{items.Description}</p>
+          <Link to={editLink}>Edit Me</Link>
+          <Link to={singleItem}>Single me out</Link>
         </div>
       </div>
     );
